7078f4bc8219201a32eec7d3af83eb1d6c063258
igorevich <lelik.belokonevvv@gmail.com>
2020-10-25 16:50:01 +0300

    test02-01

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
 const App = () => {
   return (
-    <div className="App">
-      <h1>
-        <Header />
-      </h1>
-      <h2>
-        <Texnologes />
-      </h2>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          IT Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
-const Header = () => {
-  return (
-    <div>
-      <a>Home</a>
-      <a>Message</a>
-      <a>From</a>
-    </div>
-  );
-};
-
-const Texnologes = () => {
-  return (
-    <div>
-      <ul>
-        <li>techologiest3</li>
-        <li>techologiest2</li>
-        <li>techologiest1</li>
-      </ul>
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () =>{
+    return (
+      <nav className="nav">
+      <div>
+        <a>Mesages</a>
+      </div>
+
+      <div>
+        <a>News</a>
+      </div>
+
+      <div>
+        <a>Profile</a>
+      </div>
+
+      <div>
+        <a>Music</a>
+      </div>
+    </nav>
+    ) 
+}
+export default Nav;
